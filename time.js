eeed579250e637cf572fff68236d936a14f1eb69
kosuke-kumapooh <51449617+kosuke-kumapooh@users.noreply.github.com>
2020-08-06 21:17:58 +0900

    first commit

---
+++ time.js
@@ -0,0 +1,49 @@
+var lunchStart = 12 * 60 + 15;
+var lunchEnd = 13 * 60;
+function clearNumber1() {
+  document.timerForm1.reset();
+}
+function calcTime1() {
+  var t1 = eval(timerForm1.hour1.value) * 60 +
+    eval(timerForm1.minute1.value);
+  var t2 = eval(timerForm1.hour2.value) * 60 +
+    eval(timerForm1.minute2.value);
+  if (t1 < lunchStart && t2 > lunchEnd) {
+    // お昼休み補正
+    timerForm1.result.value = (t2 - t1) / 60.0 - 1.0;
+  } else {
+    timerForm1.result.value = (t2 - t1) / 60.0;
+  }
+}
+
+function clearNumber2() {
+  document.timerForm2.reset();
+}
+function calcTime2() {
+  var t1 = eval(timerForm2.hour1.value) * 60 +
+    eval(timerForm2.minute1.value);
+  var t2 = eval(timerForm2.hour2.value) * 60 +
+    eval(timerForm2.minute2.value);
+  if (t1 < lunchStart && t2 > lunchEnd) {
+    // お昼休み補正
+    timerForm2.result.value = (t2 - t1) / 60.0 - 1.0;
+  } else {
+    timerForm2.result.value = (t2 - t1) / 60.0;
+  }
+}
+
+function clearNumber3() {
+  document.timerForm3.reset();
+}
+function calcTime3() {
+  var t1 = eval(timerForm3.hour1.value) * 60 +
+    eval(timerForm3.minute1.value);
+  var t2 = eval(timerForm3.hour2.value) * 60 +
+    eval(timerForm3.minute2.value);
+  if (t1 < lunchStart && t2 > lunchEnd) {
+    // お昼休み補正
+    timerForm3.result.value = (t2 - t1) / 60.0 - 1.0;
+  } else {
+    timerForm3.result.value = (t2 - t1) / 60.0;
+  }
+}
