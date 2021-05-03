cb18ad7ce72c381805acb1530c2cae32c6da3917
kosuke-kumapooh <koutyan.kumakuma@gmail.com>
2021-05-03 22:25:42 +0900

    1.0->2.0

--- docs/time.js
+++ docs/time.js
@@ -3,9 +3,23 @@ window.onload = function () {
 }
 
 function setLunchTime() {
-  lunchStart = eval(lunchStartHour.value) * 60 + eval(lunchStartMinute.value);
-  lunchEnd = eval(lunchEndHour.value) * 60 + eval(lunchEndMinute.value);
+  let [break1StartHour, break1StartMinute] = break1StartIn.value.split(':')
+  break1Start = eval(break1StartHour) * 60 + eval(break1StartMinute);
+  let [break1EndHour, break1EndMinute] = break1EndIn.value.split(':')
+  break1End = eval(break1EndHour) * 60 + eval(break1EndMinute);
+  break1Time = (break1End - break1Start) / 60;
+
+  let [lunchStartHour, lunchStartMinute] = lunchStartIn.value.split(':')
+  lunchStart = eval(lunchStartHour) * 60 + eval(lunchStartMinute);
+  let [lunchEndHour, lunchEndMinute] = lunchEndIn.value.split(':')
+  lunchEnd = eval(lunchEndHour) * 60 + eval(lunchEndMinute);
   lunchTime = (lunchEnd - lunchStart) / 60;
+
+  let [break2StartHour, break2StartMinute] = break2StartIn.value.split(':')
+  break2Start = eval(break2StartHour) * 60 + eval(break2StartMinute);
+  let [break2EndHour, break2EndMinute] = break2EndIn.value.split(':')
+  break2End = eval(break2EndHour) * 60 + eval(break2EndMinute);
+  break2Time = (break2End - break2Start) / 60;
 }
 
 function clearNumber(formnum) {
@@ -17,30 +31,29 @@ function calcTime(formnum) {
   var t1 = NaN;
   var t2 = NaN;
 
-  if (checkInt(formnum.hour1.value) &&
-    checkInt(formnum.minute1.value) &&
-    checkInt(formnum.hour2.value) &&
-    checkInt(formnum.minute2.value)) {
-    t1 = eval(formnum.hour1.value) * 60 +
-      eval(formnum.minute1.value);
-    t2 = eval(formnum.hour2.value) * 60 +
-      eval(formnum.minute2.value);
-  } else {
-    formnum.result.value = NaN;
-    alert('Invalid time!');
-    return false;
-  }
+  let [startHour, startMinute] = formnum.in1.value.split(':')
+  t1 = eval(startHour) * 60 +
+    eval(startMinute);
+  let [endHour, endMinute] = formnum.in2.value.split(':')
+  t2 = eval(endHour) * 60 +
+    eval(endMinute);
 
   // 開始時刻 > 終了時刻
-  if (t1 > t2) {
-    formnum.result.value = NaN;
-    alert('Invalid time!');
-    return false;
-  }
+  // if (t1 > t2) {
+  //   formnum.result.value = NaN;
+  //   alert('Invalid time!');
+  //   return false;
+  // }
 
   if (t1 <= lunchStart && t2 >= lunchEnd) {
     // お昼休み補正
     formnum.result.value = (t2 - t1) / 60.0 - lunchTime;
+  } else if (t1 <= break1Start && t2 >= break1End) {
+    // 前休憩補正
+    formnum.result.value = (t2 - t1) / 60.0 - break1Time;
+  } else if (t1 <= break2Start && t2 >= break2End) {
+    // 後休憩補正
+    formnum.result.value = (t2 - t1) / 60.0 - break2Time;
   } else {
     formnum.result.value = (t2 - t1) / 60.0;
   }
@@ -51,10 +64,6 @@ function calcTime(formnum) {
 function calcSum() {
   sumForm.sum.value = eval(timerForm1.result.value) +
     eval(timerForm2.result.value) +
-    eval(timerForm3.result.value);
-}
-
-function checkInt(value) {
-  // 半角数字かチェック
-  return value.match(/^[0-9]+$/);
+    eval(timerForm3.result.value) +
+    eval(timerForm4.result.value);
 }
